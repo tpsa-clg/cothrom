7fdf1ca921d469a540abf4877fd276aa20fc6fb3
campioru <campioru@tcd.ie>
2025-06-20 12:42:37 +0100

    histograms
    Added code to plot relevant histograms in txt_for_MCMC.py to compare proof-of-concept testing.

--- code/combine_data.py
+++ code/combine_data.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import geopandas as gpd
 import glob
@@ -8,12 +7,12 @@ import os
 data_dir = os.path.join(*[os.path.dirname(os.path.realpath(__file__)), os.pardir, "data"])
 os.makedirs(data_dir, exist_ok=True)
 
-# Downloading necessary files if not in current directory
+# Downloading necessary files if not in data directory
 pop_file = glob.glob(os.path.join(data_dir, "F1060*.csv"))
 if pop_file:
   pop_file = pop_file[0]
 else:
-  print("CSO population data not in current directory. Downloading from https://data.cso.ie/table/F1060...")
+  print("CSO population data not in data directory. Downloading from https://data.cso.ie/table/F1060...")
   r = requests.get(r"https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.PxAPIv1/en/76/C2022P1/F1060?query=%7B%22query%22:%5B%7B%22code%22:%22STATISTIC%22,%22selection%22:%7B%22filter%22:%22item%22,%22values%22:%5B%22F1060C01%22%5D%7D%7D,%7B%22code%22:%22C02199V02655%22,%22selection%22:%7B%22filter%22:%22item%22,%22values%22:%5B%22-%22%5D%7D%7D%5D,%22response%22:%7B%22format%22:%22csv%22,%22pivot%22:null,%22codes%22:true%7D%7D")
   if r.status_code == 200:
     pop_file = os.path.join(data_dir, "F1060.csv")
@@ -41,7 +40,7 @@ for m, map in enumerate(maps):
   if geo_file:
     geo_files.append(geo_file[0])
   else:
-    print(f"Tailte Éireann {map} data not in current directory. Downloading from {file_URLs[m]}...")
+    print(f"Tailte Éireann {map} data not in data directory. Downloading from {file_URLs[m]}...")
     r = requests.get(file_URLs[m])
     if r.status_code == 200:
       geo_files.append(os.path.join(data_dir, f"{map}.geojson"))
--- code/txt_for_MCMC.py
+++ code/txt_for_MCMC.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import sys
 import os
+import matplotlib.pyplot as plt
 
 
 # Area from command line
@@ -12,8 +13,8 @@ area_name = sys.argv[3]
 
 # Reading required data
 data_dir = os.path.join(*[os.path.dirname(os.path.realpath(__file__)), os.pardir, "data"])
-area_df = pd.read_csv(os.path.join(data_dir, "ED_data.csv"), usecols=["GUID", area_type, "Population", "Neighbours"])
-area_df = area_df[area_df[area_type].isin(area_list)].sort_values(by="GUID").reset_index(drop=True)
+full_df = pd.read_csv(os.path.join(data_dir, "ED_data.csv"), usecols=["GUID", area_type, "Population", "Neighbours", "Area", "Perimeter"])
+area_df = full_df[full_df[area_type].isin(area_list)].sort_values(by="GUID").reset_index(drop=True)
 intersect = set(area_df.GUID.values)
 area_df.loc[:, "Neighbours"] = np.array([eval(i) & intersect for i in area_df.Neighbours.values], dtype=object)
 area_df.loc[:, "Neighbours"] = np.array([area_df.index[area_df["GUID"].isin(i)].tolist() for i in area_df.Neighbours.values], dtype=object)
@@ -31,3 +32,32 @@ for g, p, n in zip(area_df.GUID.values, area_df.Population.values, area_df.Neigh
   files[2].write("\n")
 for file in files:
   file.close()
+
+
+# Comparing distributions of ED population, area, perimeter, and neighbours between area of consideration and Ireland
+colours = ["#004488", "#BB5566", "#DDAA33"]
+names = ["Ireland", area_name]
+for column in ["Population", "Area", "Perimeter"]:
+  fig, ax = plt.subplots()
+  axes = [ax, ax.twinx()]
+  for d, (data, a) in enumerate(zip([full_df[column].values, area_df[column].values], axes)):
+    hist, bins = np.histogram(data, bins=np.geomspace(min(data), max(data), num=int(np.ceil(np.sqrt(len(data))))))
+    a.hist(data, bins=bins, color=colours[d], alpha=.75, label=names[d])
+  ax.set_xscale("log")
+  fig.legend(bbox_to_anchor=(1,1), bbox_transform=ax.transAxes)
+  ax.set_title(column)
+  fig.savefig(os.path.join(file_dir, f"{column}.pdf"), bbox_inches="tight")
+  plt.close(fig)
+  del fig, ax
+
+fig, ax = plt.subplots()
+nei_lens = [[len(eval(i)) for i in full_df.Neighbours.values], [len(i) for i in area_df.Neighbours.values]]
+bins = np.arange(1, max(nei_lens[0])+2, 1)
+for d, (data, a) in enumerate(zip(nei_lens, [ax, ax.twinx()])):
+  a.hist(data, bins=bins, color=colours[d], alpha=.75, label=names[d])
+fig.legend(bbox_to_anchor=(1,1), bbox_transform=ax.transAxes)
+ax.set_title("Neighbours")
+fig.savefig(os.path.join(file_dir, "Neighbours.pdf"), bbox_inches="tight")
+plt.close("all")
+del fig, ax
+
