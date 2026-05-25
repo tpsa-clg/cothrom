a0d27ef3f487850d86df4a4a94f4a624938d42be
Ruaidhrí Campion <campioru@tcd.ie>
2026-05-25 20:00:53 +0100

    actual config
    Making CSV for true configuration, to include in Pareto front plots.

--- code/txt_for_MCMC.py
+++ code/txt_for_MCMC.py
@@ -13,7 +13,7 @@ area_name = sys.argv[3]
 
 # Reading required data
 data_dir = os.path.join(*[os.path.dirname(os.path.realpath(__file__)), os.pardir, "data"])
-full_df = pd.read_csv(os.path.join(data_dir, "ED_data.csv"), usecols=["GUID", area_type, "Population", "Neighbours", "County", "Area", "Perimeter"])
+full_df = pd.read_csv(os.path.join(data_dir, "ED_data.csv"), usecols=["GUID", area_type, "Population", "Neighbours", "County", "Area", "Perimeter", "Constituency"])
 area_df = full_df[full_df[area_type].isin(area_list)].reset_index(drop=True)
 intersect = set(area_df.GUID.values)
 area_df.loc[:, "Neighbours"] = np.array([eval(i) & intersect for i in area_df.Neighbours.values], dtype=object)
@@ -22,9 +22,9 @@ county_dict = {county: i for i, county in enumerate(sorted(set(area_df.County.va
 
 
 # Saving .txt files
-file_dir = os.path.join(data_dir, area_name)
-os.makedirs(file_dir, exist_ok=True)
-files = [open(os.path.join(file_dir, f"{column}.txt"), "w") for column in ["GUID", "Population", "Neighbours", "County"]]
+area_dir = os.path.join(data_dir, area_name)
+os.makedirs(area_dir, exist_ok=True)
+files = [open(os.path.join(area_dir, f"{column}.txt"), "w") for column in ["GUID", "Population", "Neighbours", "County"]]
 for g, p, n, c in zip(area_df.GUID.values, area_df.Population.values, area_df.Neighbours.values, area_df.County.values):
   files[0].write(f"{g}\n")
   files[1].write(f"{p}\n")
@@ -34,6 +34,24 @@ for g, p, n, c in zip(area_df.GUID.values, area_df.Population.values, area_df.Ne
   files[3].write(f"{county_dict[c]}\n")
 for file in files:
   file.close()
+
+
+# Making csv for actual configuration
+constituency_list = set(area_df.Constituency.values)
+constituency_df = pd.read_csv(os.path.join(data_dir, "Constituency_data.csv"))
+constituency_df = constituency_df[constituency_df["Constituency"].isin(constituency_list)]
+constituency_df.sort_values("Seats", inplace=True)
+constituency_df.reset_index(drop=True, inplace=True)
+constituency_list, seat_list = constituency_df.Constituency, constituency_df.Seats
+area_df["Configuration"] = -1
+for q, constituency in enumerate(constituency_list):
+  area_df.loc[area_df.Constituency==constituency, "Configuration"] = q
+seats, constituencies = sum(seat_list), len(seat_list)
+run_dir = os.path.join(area_dir, f"{seats}_{constituencies}")
+os.makedirs(run_dir, exist_ok=True)
+with open(os.path.join(run_dir, "actual.csv"), "w") as f:
+  f.write(f"Q,{','.join([str(seat) for seat in seat_list])}\n")
+  f.write(f"{','.join([str(q) for q in area_df["Configuration"]])}\n")
 
 
 # Comparing distributions of ED population, area, perimeter, and neighbours between area of consideration and Ireland
@@ -48,7 +66,7 @@ for column in ["Population", "Area", "Perimeter"]:
   ax.set_xscale("log")
   fig.legend(bbox_to_anchor=(1,1), bbox_transform=ax.transAxes)
   ax.set_title(column)
-  fig.savefig(os.path.join(file_dir, f"{column}.pdf"), bbox_inches="tight")
+  fig.savefig(os.path.join(area_dir, f"{column}.pdf"), bbox_inches="tight")
   plt.close(fig)
   del fig, ax
 
@@ -59,6 +77,6 @@ for d, (data, a) in enumerate(zip(nei_lens, [ax, ax.twinx()])):
   a.hist(data, bins=bins, color=colours[d], alpha=.75, label=names[d])
 fig.legend(bbox_to_anchor=(1,1), bbox_transform=ax.transAxes)
 ax.set_title("Neighbours")
-fig.savefig(os.path.join(file_dir, "Neighbours.pdf"), bbox_inches="tight")
+fig.savefig(os.path.join(area_dir, "Neighbours.pdf"), bbox_inches="tight")
 plt.close("all")
 del fig, ax
