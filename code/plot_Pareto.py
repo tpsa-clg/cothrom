ab2aa3ffdda8ca52f1c03c9efa47f46f4c5d822e
Ruaidhrí Campion <campioru@tcd.ie>
2026-05-27 02:34:53 +0100

    seat colours
    Colouring Pareto points by seat configuration.

--- code/plot_Pareto.py
+++ code/plot_Pareto.py
@@ -12,11 +12,12 @@ area_dir = os.path.join(data_dir, area)
 Pareto_dir = os.path.join(area_dir, f"{seats}_{constituencies}")
 
 config_files = glob(os.path.join(Pareto_dir, "*.csv"))
-# TODO split and colour tuples by seat configuration - get all seat configs from filename and sort by dict
-optimal_tuples = set()
+seat_configs = {config_file.split("/")[-1].split("_")[0] for config_file in config_files}
+optimal_tuples = {seat_config: set() for seat_config in seat_configs}
 for config_file in config_files:
     with open(config_file) as f:
-        line = [""]
+        line = f.readline().replace("\n", "")
+        seat_config = line[2:]
         while line[0] != "optimals":
             line = f.readline().replace("\n", "").split(",")
         line = f.readline().replace("\n", "").split(",")
@@ -24,29 +25,34 @@ for config_file in config_files:
             Hs = [float(H) for H in line[1:]]
             # TODO save non-contiguous points, make transparent
             if Hs[1] == 0:
-                optimal_tuples.add((Hs[0], Hs[2]))
+                optimal_tuples[seat_config].add((Hs[0], Hs[2]))
             next(f)
             line = f.readline().replace("\n", "").split(",")
+
 actual_file = os.path.join(area_dir, "actual.csv")
+actual_tuple = tuple()
 with open(actual_file) as f:
-    line = f.readline().replace("\n", "").split(",")
-    seat_list = [int(s) for s in line[1:]]
+    line = f.readline().replace("\n", "")
+    actual_seat_config = line[2:]
+    seat_list = [int(s) for s in line.split(",")[1:]]
     if sum(seat_list) == seats and len(seat_list) == constituencies:
         next(f)
         line = f.readline().replace("\n", "").split(",")
         Hs = [float(H) for H in line[1:]]
         actual_tuple = (Hs[0], Hs[2])
-    else:
-        print(seat_list)
-        actual_tuple = tuple()
+
+colours = ["#004488", "#BB5566", "#DDAA33", "k"]
+colour_dict = {seat_config: colour for seat_config, colour in zip(seat_configs, colours)}
 # TODO find and mark Pareto front - big markers for front points?
 if actual_tuple:
-    plt.scatter(actual_tuple[0], actual_tuple[1], marker="*")
-optimal_xs = [optimal[0] for optimal in optimal_tuples]
-optimal_ys = [optimal[1] for optimal in optimal_tuples]
-plt.scatter(optimal_xs, optimal_ys, marker=".")
+    plt.scatter(actual_tuple[0], actual_tuple[1], marker="*", color=colour_dict[actual_seat_config])
+for seat_config in seat_configs:
+    optimal_xs = [optimal[0] for optimal in optimal_tuples[seat_config]]
+    optimal_ys = [optimal[1] for optimal in optimal_tuples[seat_config]]
+    plt.scatter(optimal_xs, optimal_ys, marker=".", color=colour_dict[seat_config], label=seat_config)
 plt.xlabel(r"$H_P$")
 plt.xscale("log")
 plt.ylabel(r"$H_D$")
+plt.legend()
 # TODO label/identify front points
 plt.savefig(os.path.join(Pareto_dir, "Pareto.pdf"), bbox_inches="tight")
