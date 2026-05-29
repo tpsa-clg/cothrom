d74a93ac98461e2b8286eb932841e728ecb0eddd
Ruaidhrí Campion <campioru@tcd.ie>
2026-05-29 21:28:11 +0100

    better Pareto slurm
    Updated the Pareto slurm script for setting everything up and looping over parameters.

--- code/Pareto.sh
+++ code/Pareto.sh
@@ -1,21 +1,40 @@
 #!/bin/sh
-#SBATCH -N 1
-#SBATCH -t 4-00:00:00
-#SBATCH -p compute
-#SBATCH -J grid
-#SBATCH -a 0-25
 
 base_dir=/home/users/campioru/Electoral_Redistricting
 code_dir=${base_dir}/code
 data_dir=${base_dir}/data
 
+area_type=County
+area_list=CORK
+area_name=Cork
+
+seats=20
+constituencies=5
+declare -a seat_configs=(3,3,4,5,5 3,4,4,4,5 4,4,4,4,4)
+
+JC_proportion=0.5
+declare -a JDs=($(seq 0 0.1 2.5))
+declare -a JBs=(0)
+
+N_meas=100
+N_disc=50
+
+
 cd $base_dir
 make ED_data.csv MCMC_SA actual_H
-python3 code/txt_for_MCMC.py County CORK Cork
-mkdir ${data_dir}/Cork/20_5 -p
+python3 ${code_dir}/txt_for_MCMC.py $area_type $area_list $area_name
+mkdir ${data_dir}/${area_name}/${seats}_${constituencies} -p
+./actual_H $area_name
 OMP_SET_NUM_THREADS=16
 
-JDs=($(seq 0 0.1 2.5))
-JD=${JDs[$SLURM_ARRAY_TASK_ID]}
-JC=$(bc <<< $JD+1)
-./MCMC_SA Cork 3,3,4,5,5 ${JC},${JD},0 100,50
+for seat_config in ${seat_configs[@]}
+do
+  for JD in ${JDs[@]}
+  do
+    for JB in ${JBs[@]}
+    do
+      JC=$(bc <<< "scale=3; (${JD}+${JB}+1)*${JC_proportion}/(1-${JC_proportion})")
+      sbatch -N 1 -t 4-00:00:00 -p compute -J Pareto --wrap="./MCMC_SA $area_name $seat_config ${JC},${JD},${JB} ${N_meas},${N_disc}"
+    done
+  done
+done
